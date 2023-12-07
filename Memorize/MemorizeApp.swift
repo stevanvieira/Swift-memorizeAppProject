fdc4726829493d2b00cc40026e616c546c8de458
stevanvieira <stevanacupuncture@gmail.com>
2023-12-07 14:06:27 +0000

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by stevan on 07/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
